e3112217bd60c56d0e861b9cf125c805930b96fa
Brady Walters <brady.walters@gmail.com>
2020-10-03 12:56:27 -0500

    deployment test, removed app.run from app.py.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
 def home():
-    return 'Hello World BWCraps! Now on Azure!'
+    return 'Hello World CrapsJack! Now on Azure!'
 
-
+"""
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8000')
+"""
--- config.py
+++ config.py
@@ -1,6 +1,25 @@
 import os
+from urllib import parse
 from decouple import config
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_User = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
+
+def _build_db_connection_string() -> str:
+    db_user = config('db_user')
+    db_password = config('db_password')
+    db_server = config('db_server')
+    db_name = config('db_name')
+    params = parse.quote_plus(
+        r'Driver={ODBC Driver 17 for SQL Server};Server=' + db_server +
+        ';Database=' + db_name +
+        ';Uid=' + db_user +
+        ';Pwd=' + db_password +
+        ';Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=5;')
+
+    connection_string = f'mssql+pyodbc:///?odbc_connect={params}'
+    return connection_string
+
+
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = _build_db_connection_string()
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
